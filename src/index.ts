733e192bb38851ffdd5c45d4af5d463ca847edb9
Mike <trikefx@live.co.uk>
2024-07-14 14:57:37 +0100

    ~ Changed logic to try and stay connected.

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,7 @@ dotenv.config()
 const config = parse(readFileSync("./config.yaml", "utf8"))
 const patterns = (config?.patterns as string[] ?? []).map(p => new RegExp(p))
 
-const job = async () => {
-  console.log(`Running Email Cleaner CRON job at ${DateTime.local().toISO({ suppressMilliseconds: true, includeOffset: false })}`)
-
+const init = async () => {
   const client = new ImapFlow({
     host: "outlook.office365.com",
     port: 993,
@@ -26,10 +24,18 @@ const job = async () => {
   })
 
   await client.connect()
-  await checkJunkMailbox(client, patterns)
-  await checkEmailCleanerMailbox(client)
-  await client.logout()
+  
+  const job = async () => {
+    console.log(`Running Email Cleaner CRON job at ${DateTime.local().toISO({ suppressMilliseconds: true, includeOffset: false })}`)
+    console.log(process.env.EMAIL, process.env.PASS)
+  
+    await checkJunkMailbox(client, patterns)
+    await checkEmailCleanerMailbox(client)
+    // await client.logout()
+  }
+  
+  //job()
+  Cron("*/30 * * * *", job)
 }
 
-// job()
-Cron("*/30 * * * *", job)
+init()
